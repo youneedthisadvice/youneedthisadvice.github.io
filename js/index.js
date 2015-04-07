77fd12ce20794966443a35419c171e0a50e99881
YNTA <john.peter.gamma@gmail.com>
2015-04-07 18:27:40 +0300

    Pre-Launch Page-5
    Fixed Alignment Problem with Chrome. Replaced Font arrow with png

--- js/index.js
+++ js/index.js
@@ -11,18 +11,20 @@ function em1() {
 $(document).ready(function() {
 r = function() {
 if($(window).width() >= 960) {
-$('.img').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/ynta-303.png' : 'images/ynta-202.png') : 'images/ynta-101.png');
+$('.img').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/ynta-300.png' : 'images/ynta-200.png') : 'images/ynta-100.png');
 $('.img-2').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/yc-90.jpg' : 'images/yc-60.jpg') : 'images/yc-30.jpg');
 $('.img-3').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/stanford_big-90.png' : 'images/stanford_big-60.png') : 'images/stanford_big-30.png');
 $('.img-4').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/group-318.png' : 'images/group-212.png') : 'images/group-106.png');
-$('.img-5').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/apple-iphone-8-3x-837.png' : 'images/apple-iphone-8-3x-558.png') : 'images/apple-iphone-8-3x-279.png');
+$('.img-5').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/apple-iphone-8-3x-825.png' : 'images/apple-iphone-8-3x-550.png') : 'images/apple-iphone-8-3x-275.png');
 var a='data-lazy'; if($('.gallery .slide0').hasAttr('src')) { a='src'; } $('.gallery .slide0').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/slide1-777.png' : 'images/slide1-518.png') : 'images/slide1-259.png');
 var a='data-lazy'; if($('.gallery .slide1').hasAttr('src')) { a='src'; } $('.gallery .slide1').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/slide2-777.png' : 'images/slide2-518.png') : 'images/slide2-259.png');
 var a='data-lazy'; if($('.gallery .slide2').hasAttr('src')) { a='src'; } $('.gallery .slide2').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/slide3-777.png' : 'images/slide3-518.png') : 'images/slide3-259.png');
 var a='data-lazy'; if($('.gallery .slide3').hasAttr('src')) { a='src'; } $('.gallery .slide3').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/slide4-777.png' : 'images/slide4-518.png') : 'images/slide4-259.png');
 var a='data-lazy'; if($('.gallery .slide4').hasAttr('src')) { a='src'; } $('.gallery .slide4').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/slide5-777.png' : 'images/slide5-518.png') : 'images/slide5-259.png');
-$('.img-6').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/status-bar-white-min-720.png' : 'images/status-bar-white-min-480.png') : 'images/status-bar-white-min-240.png');
-$('.img-7').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/pigeon_friendly_720px_trans-93.png' : 'images/pigeon_friendly_720px_trans-62.png') : 'images/pigeon_friendly_720px_trans-31.png');
+$('.img-6').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/status-bar-white-min-708.png' : 'images/status-bar-white-min-472.png') : 'images/status-bar-white-min-236.png');
+$('.img-7').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/arrow-right-2-60.png' : 'images/arrow-right-2-40.png') : 'images/arrow-right-2-20.png');
+$('.img-8').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/pigeon_friendly_720px_trans-93.png' : 'images/pigeon_friendly_720px_trans-62.png') : 'images/pigeon_friendly_720px_trans-31.png');
+$('.img-9').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/arrow-right-2-162.png' : 'images/arrow-right-2-108.png') : 'images/arrow-right-2-54-2.png');
 } else {
 $('.img').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/ynta-162.png' : 'images/ynta-108.png') : 'images/ynta-54.png');
 $('.img-2').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/yc-57.jpg' : 'images/yc-38.jpg') : 'images/yc-19.jpg');
@@ -35,7 +37,9 @@ var a='data-lazy'; if($('.gallery .slide2').hasAttr('src')) { a='src'; } $('.gal
 var a='data-lazy'; if($('.gallery .slide3').hasAttr('src')) { a='src'; } $('.gallery .slide3').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/slide4-648.png' : 'images/slide4-432.png') : 'images/slide4-216.png');
 var a='data-lazy'; if($('.gallery .slide4').hasAttr('src')) { a='src'; } $('.gallery .slide4').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/slide5-648.png' : 'images/slide5-432.png') : 'images/slide5-216.png');
 $('.img-6').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/status-bar-white-min-606.png' : 'images/status-bar-white-min-404.png') : 'images/status-bar-white-min-202.png');
-$('.img-7').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/pigeon_friendly_720px_trans-90.png' : 'images/pigeon_friendly_720px_trans-60.png') : 'images/pigeon_friendly_720px_trans-30.png');
+$('.img-7').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/arrow-right-2-21.png' : 'images/arrow-right-2-14.png') : 'images/arrow-right-2-7.png');
+$('.img-8').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/pigeon_friendly_720px_trans-90.png' : 'images/pigeon_friendly_720px_trans-60.png') : 'images/pigeon_friendly_720px_trans-30.png');
+$('.img-9').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/arrow-right-2-54.png' : 'images/arrow-right-2-36.png') : 'images/arrow-right-2-18.png');
 }
 };
 $(window).resize(r);
