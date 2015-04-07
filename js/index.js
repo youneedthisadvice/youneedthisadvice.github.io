879a0229883fc5f6f4eb85e58ffe4018743136c7
YNTA <john.peter.gamma@gmail.com>
2015-04-07 17:27:19 +0300

    Pre-Launch Page-4
    Overhaul to the slideshow. Permanent fix

--- js/index.js
+++ js/index.js
@@ -16,31 +16,29 @@ $('.img-2').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRati
 $('.img-3').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/stanford_big-90.png' : 'images/stanford_big-60.png') : 'images/stanford_big-30.png');
 $('.img-4').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/group-318.png' : 'images/group-212.png') : 'images/group-106.png');
 $('.img-5').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/apple-iphone-8-3x-837.png' : 'images/apple-iphone-8-3x-558.png') : 'images/apple-iphone-8-3x-279.png');
-var a='data-lazy'; if($('.gallery .slide0').hasAttr('src')) { a='src'; } $('.gallery .slide0').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/slide6-696.png' : 'images/slide6-464.png') : 'images/slide6-232.png');
-var a='data-lazy'; if($('.gallery .slide1').hasAttr('src')) { a='src'; } $('.gallery .slide1').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/slide7-696.png' : 'images/slide7-464.png') : 'images/slide7-232.png');
-var a='data-lazy'; if($('.gallery .slide2').hasAttr('src')) { a='src'; } $('.gallery .slide2').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/slide8-696.png' : 'images/slide8-464.png') : 'images/slide8-232.png');
-var a='data-lazy'; if($('.gallery .slide3').hasAttr('src')) { a='src'; } $('.gallery .slide3').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/slide10-696.png' : 'images/slide10-464.png') : 'images/slide10-232.png');
-var a='data-lazy'; if($('.gallery .slide4').hasAttr('src')) { a='src'; } $('.gallery .slide4').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/slide9-696.png' : 'images/slide9-464.png') : 'images/slide9-232.png');
+var a='data-lazy'; if($('.gallery .slide0').hasAttr('src')) { a='src'; } $('.gallery .slide0').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/slide1-777.png' : 'images/slide1-518.png') : 'images/slide1-259.png');
+var a='data-lazy'; if($('.gallery .slide1').hasAttr('src')) { a='src'; } $('.gallery .slide1').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/slide2-777.png' : 'images/slide2-518.png') : 'images/slide2-259.png');
+var a='data-lazy'; if($('.gallery .slide2').hasAttr('src')) { a='src'; } $('.gallery .slide2').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/slide3-777.png' : 'images/slide3-518.png') : 'images/slide3-259.png');
+var a='data-lazy'; if($('.gallery .slide3').hasAttr('src')) { a='src'; } $('.gallery .slide3').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/slide4-777.png' : 'images/slide4-518.png') : 'images/slide4-259.png');
+var a='data-lazy'; if($('.gallery .slide4').hasAttr('src')) { a='src'; } $('.gallery .slide4').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/slide5-777.png' : 'images/slide5-518.png') : 'images/slide5-259.png');
+$('.img-6').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/status-bar-white-min-720.png' : 'images/status-bar-white-min-480.png') : 'images/status-bar-white-min-240.png');
 $('.img-7').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/pigeon_friendly_720px_trans-93.png' : 'images/pigeon_friendly_720px_trans-62.png') : 'images/pigeon_friendly_720px_trans-31.png');
 } else {
 $('.img').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/ynta-162.png' : 'images/ynta-108.png') : 'images/ynta-54.png');
 $('.img-2').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/yc-57.jpg' : 'images/yc-38.jpg') : 'images/yc-19.jpg');
 $('.img-3').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/stanford_big-57.png' : 'images/stanford_big-38.png') : 'images/stanford_big-19.png');
 $('.img-4').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/group-228.png' : 'images/group-152.png') : 'images/group-76.png');
-$('.img-5').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/apple-iphone-8-3x-681.png' : 'images/apple-iphone-8-3x-454.png') : 'images/apple-iphone-8-3x-227.png');
-var a='data-lazy'; if($('.gallery .slide0').hasAttr('src')) { a='src'; } $('.gallery .slide0').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/slide6-600.png' : 'images/slide6-400.png') : 'images/slide6-200.png');
-var a='data-lazy'; if($('.gallery .slide1').hasAttr('src')) { a='src'; } $('.gallery .slide1').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/slide7-600.png' : 'images/slide7-400.png') : 'images/slide7-200.png');
-var a='data-lazy'; if($('.gallery .slide2').hasAttr('src')) { a='src'; } $('.gallery .slide2').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/slide8-600.png' : 'images/slide8-400.png') : 'images/slide8-200.png');
-var a='data-lazy'; if($('.gallery .slide3').hasAttr('src')) { a='src'; } $('.gallery .slide3').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/slide10-600.png' : 'images/slide10-400.png') : 'images/slide10-200.png');
-var a='data-lazy'; if($('.gallery .slide4').hasAttr('src')) { a='src'; } $('.gallery .slide4').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/slide9-600.png' : 'images/slide9-400.png') : 'images/slide9-200.png');
+$('.img-5').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/apple-iphone-8-3x-714.png' : 'images/apple-iphone-8-3x-476.png') : 'images/apple-iphone-8-3x-238.png');
+var a='data-lazy'; if($('.gallery .slide0').hasAttr('src')) { a='src'; } $('.gallery .slide0').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/slide1-648.png' : 'images/slide1-432.png') : 'images/slide1-216.png');
+var a='data-lazy'; if($('.gallery .slide1').hasAttr('src')) { a='src'; } $('.gallery .slide1').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/slide2-648.png' : 'images/slide2-432.png') : 'images/slide2-216.png');
+var a='data-lazy'; if($('.gallery .slide2').hasAttr('src')) { a='src'; } $('.gallery .slide2').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/slide3-648.png' : 'images/slide3-432.png') : 'images/slide3-216.png');
+var a='data-lazy'; if($('.gallery .slide3').hasAttr('src')) { a='src'; } $('.gallery .slide3').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/slide4-648.png' : 'images/slide4-432.png') : 'images/slide4-216.png');
+var a='data-lazy'; if($('.gallery .slide4').hasAttr('src')) { a='src'; } $('.gallery .slide4').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/slide5-648.png' : 'images/slide5-432.png') : 'images/slide5-216.png');
+$('.img-6').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/status-bar-white-min-606.png' : 'images/status-bar-white-min-404.png') : 'images/status-bar-white-min-202.png');
 $('.img-7').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/pigeon_friendly_720px_trans-90.png' : 'images/pigeon_friendly_720px_trans-60.png') : 'images/pigeon_friendly_720px_trans-30.png');
 }
 };
 $(window).resize(r);
 r();
-if(! document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image", "1.1")) {
-$('.img-6').attr('src', 'images/bgstat-200.png');
-
-}
-$('.gallery .slider').slick({lazyLoad: 'ondemand',slidesToShow: 1,slidesToScroll: 1,speed: 200,dots: false,arrows: false,infinite: false,autoplay: true,autoplaySpeed: 4500});
+$('.gallery .slider').slick({lazyLoad: 'ondemand',slidesToShow: 1,slidesToScroll: 1,fade: true,slide: 'div',cssEase: 'linear',speed: 100,dots: false,arrows: false,infinite: false,autoplay: true,autoplaySpeed: 5500});
 });
