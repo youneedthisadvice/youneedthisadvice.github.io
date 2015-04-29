084fa5517eb7c015f4d8da2283f8ca48932b2753
YNTA <john.peter.gamma@gmail.com>
2015-04-29 11:54:22 +0300

    New & Updated
    Added SM buttons

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
 r = function() {
 if($(window).width() >= 960) {
 $('.img').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/logo-3x-min-210.png' : 'images/logo-3x-min-140.png') : 'images/logo-3x-min-70.png');
-$('.img-2').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/pigeon-3x-48.png' : 'images/pigeon-3x-32.png') : 'images/pigeon-3x-16-2.png');
+$('.img-2').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/pigeon-3x-48.png' : 'images/pigeon-3x-32.png') : 'images/pigeon-3x-16.png');
 $('.img-3').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/line-devices-3x-min-84.png' : 'images/line-devices-3x-min-56.png') : 'images/line-devices-3x-min-28.png');
 $('.img-4').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/apple-iphone-8-3x-min-1260.png' : 'images/apple-iphone-8-3x-min-840.png') : 'images/apple-iphone-8-3x-min-420-2.png');
 var a='data-lazy'; if($('.gallery .slide0').hasAttr('src')) { a='src'; } $('.gallery .slide0').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/ph-slide_1_min-1140.png' : 'images/ph-slide_1_min-760.png') : 'images/ph-slide_1_min-380.png');
@@ -24,8 +24,8 @@ var a='data-lazy'; if($('.gallery .slide5').hasAttr('src')) { a='src'; } $('.gal
 var a='data-lazy'; if($('.gallery .slide6').hasAttr('src')) { a='src'; } $('.gallery .slide6').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/ph-slide_7_min-1140.png' : 'images/ph-slide_7_min-760.png') : 'images/ph-slide_7_min-380.png');
 $('.img-5').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/apple-iphone-8-path-3-162.png' : 'images/apple-iphone-8-path-3-108.png') : 'images/apple-iphone-8-path-3-54-2.png');
 } else {
-$('.img').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/logo-3x-min-69.png' : 'images/logo-3x-min-46.png') : 'images/logo-3x-min-23.png');
-$('.img-2').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/pigeon-3x-24.png' : 'images/pigeon-3x-16.png') : 'images/pigeon-3x-8.png');
+$('.img').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/logo-3x-min-93.png' : 'images/logo-3x-min-62.png') : 'images/logo-3x-min-31.png');
+$('.img-2').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/pigeon-3x-33.png' : 'images/pigeon-3x-22.png') : 'images/pigeon-3x-11.png');
 $('.img-3').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/line-devices-3x-min-27.png' : 'images/line-devices-3x-min-18.png') : 'images/line-devices-3x-min-9.png');
 $('.img-4').attr('src', (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/apple-iphone-8-3x-min-420.png' : 'images/apple-iphone-8-3x-min-280.png') : 'images/apple-iphone-8-3x-min-140.png');
 var a='data-lazy'; if($('.gallery .slide0').hasAttr('src')) { a='src'; } $('.gallery .slide0').attr(a, (window.devicePixelRatio > 1) ? ((window.devicePixelRatio > 2) ? 'images/ph-slide_1_min-381.png' : 'images/ph-slide_1_min-254.png') : 'images/ph-slide_1_min-127.png');
